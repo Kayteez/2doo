c4852d94fdfdccd1517969f136814dbc57a251d9
prd-phat-vu <phat.vu@paradox.ai>
2024-10-04 21:48:15 +0700

    feat: today

--- src/pages/today/index.tsx
+++ src/pages/today/index.tsx
@@ -1,5 +1,4 @@
 import ImagePlanToday from '~/assets/images/svg/nguoi-chien-soan-plan.svg'
-import JobCard from './components/JobCard'
 import Tag from './components/Tag'
 import ScheduleSVG from '~/components/icons/schedule'
 import PrioritySVG from '~/components/icons/priority'
@@ -9,8 +8,11 @@ import TagSVG from '~/components/icons/tag'
 import FlagSVG from '~/components/icons/flag'
 import SetNote from '~/assets/images/svg/set-note.svg'
 import CancelNote from '~/assets/images/svg/cancel-note.svg'
+import { useState } from 'react'
 
 const Today = () => {
+  const [jobTitle, setJobTitle] = useState('')
+  const [jobDescription, setJobDescription] = useState('')
   const listTags = [
     {
       icon: <ScheduleSVG />,
@@ -42,11 +44,47 @@ const Today = () => {
     }
   ]
 
+  const clearContent = () => {
+    setJobTitle('')
+    setJobDescription('')
+  }
+
   return (
     <div className='py-10 h-full'>
       <img src={ImagePlanToday} />
       <div className='mt-10'>
-        <JobCard />
+        <div className='bg-white border border-gray-300 rounded-3xl shadow-sm'>
+          <div className='px-4 relative border-b border-gray-300'>
+            <p
+              className={`absolute left-4 top-3 text-[#474747] text-xl font-bold transition-all duration-200 pointer-events-none ${
+                jobTitle ? 'opacity-0' : 'opacity-100'
+              }`}
+            >
+              Tên công việc
+            </p>
+            <input
+              type='text'
+              value={jobTitle}
+              onChange={(e) => {
+                setJobTitle(e.target.value)
+              }}
+              className='w-full py-3 text-gray-900 placeholder-transparent focus:outline-none focus:ring-0 text-xl font-bold'
+              placeholder='Tên công việc'
+            />
+          </div>
+          <div>
+            <textarea
+              value={jobDescription}
+              onChange={(e) => {
+                setJobDescription(e.target.value)
+              }}
+              className='w-full p-4 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-0 rounded-3xl'
+              rows={4}
+              placeholder='Mô tả công việc'
+              style={{ resize: 'none' }}
+            ></textarea>
+          </div>
+        </div>
       </div>
       <div className='mt-10 flex flex-wrap gap-4 items-center justify-end'>
         {listTags.map((item) => (
@@ -54,8 +92,20 @@ const Today = () => {
         ))}
       </div>
       <div className='absolute right-5 bottom-40 flex flex-col gap-5 cursor-pointer'>
-        <img src={CancelNote} />
-        <img src={SetNote} />
+        <img
+          src={CancelNote}
+          onClick={() => {
+            console.log(`Cancel note`)
+            clearContent()
+          }}
+        />
+        <img
+          src={SetNote}
+          onClick={() => {
+            console.log(`Set note - ${jobTitle} - ${jobDescription}`)
+            clearContent()
+          }}
+        />
       </div>
     </div>
   )
